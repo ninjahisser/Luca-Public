4e6bf31c1a17dfc77cb2359b1474ffa7730f144a
ninjahisser <majorsquidgaming.yt@gmail.com>
2024-11-22 13:32:37 +0100

    update

---
+++ website/js/micInterface.js
@@ -0,0 +1,85 @@
+const micInterface = {
+    audioContext: null,
+    analyser: null,
+    dataArray: null,
+    sampleRate: null,
+
+    sensitivity: 5,
+    initialized: false,
+
+    async initiateAudio() {
+        // Initialize audio context and analyser
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const source = this.audioContext.createMediaStreamSource(stream);
+
+        this.analyser = this.audioContext.createAnalyser();
+        this.analyser.fftSize = 2048;
+        this.sampleRate = this.audioContext.sampleRate;
+
+        const bufferLength = this.analyser.fftSize;
+        this.dataArray = new Float32Array(bufferLength);
+
+        source.connect(this.analyser);
+        this.initialized = true;
+    },
+
+    calculateLoudness() {
+        this.analyser.getFloatTimeDomainData(this.dataArray);
+        // Calculate RMS (Root Mean Square) for loudness
+        const rms = Math.sqrt(
+            this.dataArray.reduce((sum, value) => sum + value ** 2, 0) / this.dataArray.length
+        );
+        return rms;
+    },
+
+    autoCorrelate() {
+        this.analyser.getFloatTimeDomainData(this.dataArray);
+
+        const buffer = this.dataArray;
+        const size = buffer.length;
+        const maxOffset = Math.floor(size / 2);
+        let bestOffset = -1;
+        let bestCorrelation = 0;
+
+        for (let offset = 1; offset < maxOffset; offset++) {
+            let correlation = 0;
+
+            for (let i = 0; i < size - offset; i++) {
+                correlation += buffer[i] * buffer[i + offset];
+            }
+
+            correlation /= size - offset;
+
+            if (correlation > bestCorrelation) {
+                bestCorrelation = correlation;
+                bestOffset = offset;
+            }
+        }
+
+        // Refine offset using parabolic interpolation
+        if (bestCorrelation > 0.01 && bestOffset > 0 && bestOffset < maxOffset - 1) {
+            const y1 = bestCorrelation;
+            const y0 = buffer[bestOffset - 1] || 0;
+            const y2 = buffer[bestOffset + 1] || 0;
+            const refinedOffset = bestOffset + 0.5 * ((y0 - y2) / (y0 - 2 * y1 + y2));
+            return this.sampleRate / refinedOffset;
+        }
+
+        return -1; // No pitch detected
+    },
+
+    getVolumePercent() {
+        return Math.min(Math.max(this.calculateLoudness() * 100, 0), 100) * this.sensitivity;
+    },
+
+    getPitch() {
+        return this.autoCorrelate() * this.sensitivity;
+    },
+
+    setSensitivity(new_sensitivity){
+        this.sensitivity = new_sensitivity;
+    }
+};
+
+micInterface.initiateAudio();
---
+++ website/js/scene.js
@@ -0,0 +1,57 @@
+const elements = [
+    {
+        pos: [20, 35],
+        scale: 70,
+        image_url: "res/Cat/Body.png",
+
+        html_element: null,
+        updateTransform(micInterface){
+            loudness = micInterface.getVolumePercent() * .02;
+            this.html_element.style.transform = `scaleX(${loudness + 100}%) scaleY(${loudness*2 + 100}%)`;
+        },
+    },
+    {
+        pos: [10, 65],
+        scale: 70,
+        image_url: "res/Cat/Tail.png",
+
+        html_element: null,
+
+        rotation: 0,
+        updateTransform(micInterface){
+            loudness = micInterface.getVolumePercent() * .2 + 180;
+            this.rotation = lerp(this.rotation, loudness, .1);
+            this.html_element.style.transform = `rotateZ(${this.rotation}deg) translateX(50%)`;
+        }
+    }
+]
+
+function updateAllElements(){
+    elements.forEach(function(element) {
+        if(micInterface.initialized){
+            element.updateTransform(micInterface);
+        }
+    });
+
+    requestAnimationFrame(updateAllElements)
+}
+
+function load_scene(){
+    elements.forEach(function(element) {
+        var newElement = document.createElement("img");
+        newElement.src = element.image_url;
+        newElement.classList.add("element");
+
+        var parent = document.getElementsByTagName("figure")[0];
+        parent.appendChild(newElement);
+        element.html_element = newElement;
+
+        newElement.style.left = element.pos[0] + "%";
+        newElement.style.top = element.pos[1] + "%";
+        newElement.style.width = element.scale + "%";
+    });
+}
+
+load_scene();
+
+requestAnimationFrame(updateAllElements)
